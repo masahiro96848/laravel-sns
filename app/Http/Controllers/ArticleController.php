059c72212418658d9ecf349ff32d29908102dd8c
masahiro-g <kwoaudis32@gmail.com>
2020-07-16 20:49:30 +0900

    記事編集 削除 更新 詳細

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,11 +9,21 @@ use App\Article;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
+    }
+
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
 
         return view('articles.index', ['articles' => $articles]);
+    }
+
+    public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
     }
 
     public function create()
@@ -39,6 +49,23 @@ class ArticleController extends Controller
         $article->fill($request->all());
         $article->user_id = $request->user()->id;
         $article->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);
+    }
+
+    public function update(ArticleRequest $request,  Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function destroy (Article $article)
+    {
+        $article->delete();
         return redirect()->route('articles.index');
     }
 }
--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,9 +1,9 @@
 @csrf
 <div class="md-form">
   <label for="">タイトル</label>
-  <input type="text" name="title" class="form-control" required value="{{ old('title')}}">
+  <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
 </div>
 <div class="form-group">
   <label for=""></label>
-  <textarea name="body" class="form-control" id="" cols="30" rows="16" placeholder="本文">{{ old('body') }}</textarea>
+  <textarea name="body" class="form-control" id="" cols="30" rows="16" placeholder="本文">{{  $article->body ?? old('body') }}</textarea>
 </div>
